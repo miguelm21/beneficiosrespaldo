9cc1d0bfc397105b63d90528f782ece2c4df7632
juangonzaleze <juan.gonzalez@astrapp.com>
2018-04-26 12:09:59 -0430

    CBUD-37 Add Category and Blog views

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -1,15 +1,153 @@
 @extends('template')
 
 @section('content')
-<div class="container-fluid container-edit closet-container">
-	<div class="row">
-		<div class="col-12">
-			<div class="logo text-right"></div>
+<div class="container container-edit closet-container">
+	<div class="category-father">
+		<div class="row my-4">
+			<div class="col-lg-3 m-lg-auto d-flex col-sm-3 m-sm-auto col-12">
+				<i class="fas fa-home category-father__icon fa-3x"></i>
+				<h4 class=" category-father__title">Categorias</h4>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-12 col-12">
-			<h4>Titulo</h4>
+		<div class="row">
+			<div class="col-md-3 col-sm-6 ">
+				<div class="service-box">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/gastro_1.jpg')}}" class="img-fluid" alt="">
+							</div>
+							<h3>Gastronomia</h3>
+						</div>
+						<div class="service-content">
+							<h3>design</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 ">
+				<div class="service-box">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/tourism_1.jpg')}}" class="img-fluid" alt="">
+								<h3>Turismo</h3>
+							</div>
+						</div>
+						<div class="service-content">
+							<h3>Descripción</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box ">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/entertaiment_1.jpg')}}" class="img-fluid" alt="">
+								<h3>Moda</h3>
+							</div>
+						</div>
+						<div class="service-content">
+							<h3>Descripción</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/beauty_1.jpg')}}" class="img-fluid" alt="">
+								<h3>Belleza</h3>
+							</div>
+						</div>
+						<div class="service-content">
+							<h3>Descripción</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3 col-sm-6 ">
+				<div class="service-box">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/news_1.jpg')}}" class="img-fluid" alt="">
+								<h3>design</h3>
+							</div>
+						</div>
+						<div class="service-content">
+							<h3>Descripción</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 ">
+				<div class="service-box">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/fashion_1.jpg')}}" class="img-fluid" alt="">
+								<h3>php</h3>
+							</div>
+						</div>
+						<div class="service-content">
+							<h3>Descripción</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box ">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/home_1.jpg')}}" class="img-fluid" alt="">
+								<h3>Ui Developer</h3>
+							</div>
+						</div>
+						<div class="service-content">
+							<h3>Descripción</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="service-box">
+					<a href="">
+						<div class="service-icon background-box">
+							<div class="front-content">
+								<img src="{{asset('img/category/entertaiment_1.jpg')}}" class="img-fluid" alt="">
+								<h3>java script</h3>
+							</div>
+						</div>
+						<div class="service-content">
+							<h3>Descripción</h3>
+							<p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure</p>
+							<span class="span-box">25%</span>
+						</div>
+					</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
